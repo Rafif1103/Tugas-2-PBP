8b031f3eb849f59052a023c6e67aa7e14afcd3a5
Rafif1103 <rafifrahmadika@gmail.com>
2022-09-11 19:52:07 +0700

    Deploy 1 Tugas-2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    katalog_context = {
+        'list_item': data_barang_katalog,
+        'nama': 'Rafif Naufal Rahmadika',
+        'npm': '2106636275'
+    }
+    return render(request, "katalog.html", katalog_context)
+    
